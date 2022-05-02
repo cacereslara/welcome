1a6a3cd6a596920ed72e29349d87bf2bfe122d3a
Jesús Cáceres <cacereslaraj@gmail.com>
2022-05-01 21:43:35 -0300

    Add new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { MainComponent } from './components/main/main.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { ExperiencesComponent } from './components/experiences/experiences.component';
+import { EducationComponent } from './components/education/education.component';
+import { CoursesComponent } from './components/courses/courses.component';
+import { CardComponent } from './components/card/card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainComponent,
+    BannerComponent,
+    ExperiencesComponent,
+    EducationComponent,
+    CoursesComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule,
